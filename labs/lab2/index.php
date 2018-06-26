55d58b4afc213d1bff507da866a949e3e0b6b020
ChrisCarson <chcarson@csumb.edu>
2018-06-26 03:48:39 +0000

    lab2 finished

---
+++ labs/lab2/index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title> 777 Slot Machine </title>
+        <?php
+            include 'inc/functions.php';
+        ?>
+        <style>
+            @import url("css/styles.css");
+        </style>
+        <script type="text/javascript">
+        function play_sound() {
+        var audioElement = document.createElement('audio');
+        audioElement.setAttribute('src', 'inc/money.mp3');
+        audioElement.setAttribute('autoplay', 'autoplay');
+        audioElement.load();
+        audioElement.play();
+    }
+</script>
+    </head>
+    <body>
+        <div id="main">
+        
+        <?php
+            play();
+        ?>
+
+        <form>
+            <input type="submit" value="Spin"/>
+        </form>
+        </div>
+
+    </body>
+</html>
